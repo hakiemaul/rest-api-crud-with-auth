dc80ba4fde590f64843f10bdd53e27aa5dd49f58
hakiemaul <noersedya@gmail.com>
2017-05-27 13:42:11 +0700

    add already logged in case in auth controller, changed schema format name

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const sec = process.env.TOKEN_SECRET;
 const saltRounds = Number(process.env.SALT_ROUNDS);
 
-var company = require('../models/company');
+var Company = require('../models/company');
 var bcrypt = require('bcrypt');
 var jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
@@ -12,27 +12,32 @@ mongoose.connect('mongodb://localhost/compalink');
 var login = function(req, res, next) {
   let email = req.body.email;
   let password = req.body.password;
+  let token = req.headers.token;
 
-  company.findOne({ email: email }, function(err, company) {
-    if(err) res.send(err);
-    bcrypt.compare(password, company.password)
-    .then(result => {
-      if(result) {
-        var token = jwt.sign({id: company.id, name: company.name, role: company.role }, sec);
-        res.send(token);
-      } else {
-        res.send('Password salah');
-      }
+  if(!token) {
+    Company.findOne({ email: email }, function(err, company) {
+      if(err) res.send(err);
+      bcrypt.compare(password, company.password)
+      .then(result => {
+        if(result) {
+          var token = jwt.sign({id: company.id, name: company.name, role: company.role }, sec);
+          res.send(token);
+        } else {
+          res.send('Password salah');
+        }
+      })
+      .catch(err => console.log(err))
     })
-    .catch(err => console.log(err))
-  })
+  } else {
+    res.send('You already have a token!')
+  }
 }
 
 var signup = function(req, res, next) {
   var salt = bcrypt.genSaltSync(saltRounds);
   var hash = bcrypt.hashSync(req.body.password, salt)
 
-  var newCompany = new company({
+  var newCompany = new Company({
     name: req.body.name,
     email: req.body.email,
     password: hash,
--- controllers/companyController.js
+++ controllers/companyController.js
@@ -1,40 +1,40 @@
 require('dotenv').config();
 const saltRounds = Number(process.env.SALT_ROUNDS);
 
-var company = require('../models/company');
+var Company = require('../models/company');
 var bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/compalink');
 
 var get = function(req, res) {
-  company.find({}, function (err, companies) {
+  Company.find({}, function (err, companies) {
     res.send(companies)
   });
 }
 
 var create = function(req, res) {
-  company.create(req.body, (err, company) => {
+  Company.create(req.body, (err, company) => {
     if(err) res.send(err.errors)
     res.send(company)
   })
 }
 
 var getOne = function(req, res) {
-  company.find({_id: req.params.id}, (err, company) => {
+  Company.find({_id: req.params.id}, (err, company) => {
     res.send(company)
   })
 }
 
 var update = function(req, res) {
-  company.findByIdAndUpdate(req.params.id, { $set: req.body }, { runValidators: true }, (err, company) => {
+  Company.findByIdAndUpdate(req.params.id, { $set: req.body }, { runValidators: true }, (err, company) => {
     if(err) res.send(err.errors)
     res.send(company)
   })
 }
 
 var remove = function(req, res) {
-  company.findOneAndRemove({_id: req.params.id}, (err, company) => {
+  Company.findOneAndRemove({_id: req.params.id}, (err, company) => {
     if(err) res.send(err)
     res.send(company)
   })
